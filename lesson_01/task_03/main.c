4a66f3f1cf4ad7a75d2263e1bb88ed57de2379e4
Pavel Omelchenko <p.Omelchenko@gmail.com>
2024-02-22 22:29:15 +0300

    Добавил решение для задания 3 домашней работы 1

---
+++ lesson_01/task_03/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("     *\n"
+           "    ***\n"
+           "   *****\n"
+           "  *******\n"
+           "   HH HH\n"
+           "   ZZZZZ");
+
+    return 0;
+}
