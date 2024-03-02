5fbbcc632ae53d4b1a0b94b75abb47559aec5120
Pavel Omelchenko <p.Omelchenko@gmail.com>
2024-03-02 21:01:39 +0300

    Добавил решение для задания 5 домашней работы 3

---
+++ lesson_03/task_05/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int a, a1, a10, a100;
+    scanf("%d", &a);
+
+    a1 = a % 10;
+    a10 = a / 10 % 10;
+    a100 = a / 100 % 10;
+
+    printf("%d\n", a1 * a10 * a100);
+    return 0;
+}
