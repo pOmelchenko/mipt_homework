1e4f05c68e49d2e6316b1b076e615f3ef3d5f24b
Pavel Omelchenko <p.Omelchenko@gmail.com>
2024-03-06 16:17:57 +0300

    Добавил решение для задания 7 домашней работы 4

---
+++ lesson_04/task_07/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void) {
+    int a, b, c, d, e, max;
+    scanf("%d%d%d%d%d", &a, &b, &c, &d, &e);
+
+    max = a;
+
+    if (max < b) {
+        max = b;
+    }
+    if (max < c) {
+        max = c;
+    }
+    if (max < d) {
+        max = d;
+    }
+    if (max < e) {
+        max = e;
+    }
+
+    printf("%d", max);
+
+    return 0;
+}
