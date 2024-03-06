06b3cffa38354d2b192df2580dcf7fa1afef65b4
Pavel Omelchenko <p.Omelchenko@gmail.com>
2024-03-06 16:38:33 +0300

    Добавил решение для задания 10 домашней работы 4

---
+++ lesson_04/task_10/main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(void) {
+    int month;
+
+    scanf("%d", &month);
+
+    if (month < 1 || month > 12) {
+        printf("wrong month number\n");
+        return 1;
+    }
+
+    switch (month) {
+        // winter
+        case 12:
+        case 1:
+        case 2:
+            printf("winter\n");
+            break;
+        // spring
+        case 3:
+        case 4:
+        case 5:
+            printf("spring\n");
+            break;
+        // summer
+        case 6:
+        case 7:
+        case 8:
+            printf("summer\n");
+            break;
+        // autumn
+        case 9:
+        case 10:
+        case 11:
+            printf("autumn\n");
+            break;
+        default:
+            printf("wrong month number\n");
+            return 1;
+    }
+
+    return 0;
+}
