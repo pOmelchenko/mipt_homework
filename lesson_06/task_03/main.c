47773184490ad455e773af80fe0673939dcb92a9
Pavel Omelchenko <p.Omelchenko@gmail.com>
2024-03-21 23:27:20 +0300

    Добавил решение для задания 3 домашней работы 6

---
+++ lesson_06/task_03/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int nod(int a, int b)
+{
+    int min = a < b ? a : b;
+
+    for (int divider = min; divider >= 0; divider--) {
+        if ((a % divider == 0) && (b % divider == 0)) {
+            return divider;
+        }
+    }
+
+    return 0;
+}
+
+int main(void)
+{
+    int a, b;
+    scanf("%d%d", &a, &b);
+
+    fprintf(stdout, "NOD for a = %d and b = %d is %d", a, b, nod(a, b));
+
+    return 0;
+}
