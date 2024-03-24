af9c883718763d898eef0cf5c64c045e97441d08
Pavel Omelchenko <p.Omelchenko@gmail.com>
2024-03-24 19:39:34 +0300

    добавил решение для контеста a18

---
+++ contests/a/a18/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a, b;
+    scanf("%d%d", &a, &b);
+
+    if (a < b) {
+        printf("Less\n");
+    } else if (a > b) {
+        printf("Above\n");
+    } else {
+        printf("Equal\n");
+    }
+
+    return 0;
+}
