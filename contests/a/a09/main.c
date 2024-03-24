04bc60656b2f35cc1646f38771280442c385741e
Pavel Omelchenko <p.Omelchenko@gmail.com>
2024-03-24 18:09:43 +0300

    добавил решение для контеста a09

---
+++ contests/a/a09/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int res, nums[5];
+
+    scanf("%d%d%d%d%d", &nums[0], &nums[1], &nums[2], &nums[3], &nums[4]);
+
+    res = nums[0];
+    for (int i = 1; i < 5; i++) {
+        if (res < nums[i]) {
+            res = nums[i];
+        }
+    }
+
+    fprintf(stdout, "%d\n", res);
+
+    return 0;
+}
