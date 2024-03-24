cdd88336b03f5c2f1c388d36b01c5c2ba8b15301
Pavel Omelchenko <p.Omelchenko@gmail.com>
2024-03-24 19:39:21 +0300

    добавил решение для контеста a16

---
+++ contests/a/a16/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(void)
+{
+    int a, b, c;
+    bool asd = false;
+
+    scanf("%d%d%d", &a, &b, &c);
+
+    if (a < b) {
+        asd = true;
+    }
+
+    if (b > c) {
+        asd = false;
+    }
+
+    printf("%s\n", asd ? "YES" : "NO");
+
+    return 0;
+}
