aeb1fa2d0fd72b5af675d924df7a22eaf38178e7
Pavel Omelchenko <p.Omelchenko@gmail.com>
2024-04-03 20:44:49 +0400

    добавил решение для контеста b07

---
+++ contests/b/b07/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(void)
+{
+    int number;
+    scanf("%d", &number);
+
+    bool has_pair = false;
+    int previous = number % 10;
+    while (number /= 10) {
+        while (number /= 10) {
+            if (previous == number % 10) has_pair = true;
+        }
+        previous = number % 10;
+    }
+
+    fprintf(stdout, "%s", has_pair ? "YES" : "NO");
+}
